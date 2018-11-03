a3e56823a18675fc2ed836a404dcb3d649a8554c
tiancaidqf <792505038@qq.com>
2018-11-03 13:41:13 +0800

    commit

--- 画图2.py
+++ 画图2.py
@@ -1,24 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def u(t):
-    if t>=0
-    return 
-x=np.linspace(-np.pi,np.pi,100)
-ω=np.pi
-t0=np.pi
- x1(t)=sin(ωt)u(t)
- x2(t)=sin(ω(t-t0))
- x3(t)=sin(ωt)u(t-t0)
- x4(t)=sin(ω(t-t0))u(t-t0)
-plt.figure(num=1)
-ax = plt.gca()                                           
+t = np.linspace(-3, 3, 500)
+x1 = np.e**t
+x2 = np.e**-t
+x3 = np.e**0*t
+plt.figure()
+plt.plot(t,x1)
+plt.plot(t,x2)
+plt.plot(t,x3)
+ax = plt.gca() 
+plt.xticks([])
+plt.yticks([])                                          
 ax.spines['right'].set_color('none') 
 ax.spines['top'].set_color('none')        
 ax.xaxis.set_ticks_position('bottom')   
 ax.yaxis.set_ticks_position('left')
 ax.spines['bottom'].set_position(('data', 0))
 ax.spines['left'].set_position(('data', 0))
-ax.set_title("x1(t)=sin(ωt)u(t)")
-plt.show
-
+ax.set_title("x(t)=Ce^st")
+plt.show()
 
